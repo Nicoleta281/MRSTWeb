6403419596fc48546a47d44c18656d33de48891f
Anastasia641 <anastasia.petrusca@isa.utm.md>
2025-05-15 12:22:13 +0300

    Add files via upload

---
+++ BusinessLogic.cs
@@ -0,0 +1,19 @@
+﻿using eUseControl.BusinesLogic;
+using eUseControl.BusinesLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public ISession GetSessionBL()
+        {
+            return new SessionBL();
+        }
+    }
+}
+
---
+++ SessionBL.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration; // pentru ConfigurationManager
+using System.Linq;
+using eUseControl.BusinesLogic.DBModel.Seed;
+using eUseControl.BusinesLogic.Interfaces;
+using eUseControl.Domain.Entities.User;
+
+namespace eUseControl.BusinessLogic
+{
+     public class SessionBL : ISession
+     {
+          public UserLoginResponse UserLogin(ULoginData data)
+          {
+               UDTable user;
+
+               using (var db = new UserContext())
+               {
+                    user = db.Users.FirstOrDefault(u => u.Username == data.Credential && u.Password == data.Password);
+               }
+
+               if (user != null)
+               {
+                    return new UserLoginResponse
+                    {
+                         Status = true,
+                         StatusMsg = "Autentificare reușită!",
+                         User = user
+                    };
+               }
+
+               return new UserLoginResponse
+               {
+                    Status = false,
+                    StatusMsg = "Credentiale invalide!"
+               };
+          }
+     }
+}
