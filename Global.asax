dd2c3d9501ace66f05e27fb8706d1ce43bbfeb69
Anastasia641 <anastasia.petrusca@isa.utm.md>
2025-05-15 12:30:03 +0300

    Add files via upload

--- Global.asax
+++ Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication1.Global" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication4.Global" Language="C#" %>
--- Global.asax.cs
+++ Global.asax.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
-namespace WebApplication1
+using System.Web.SessionState;
+using System.Web.Optimization;
+namespace WebApplication4
 {
     public class Global : HttpApplication
     {
@@ -16,6 +16,7 @@ namespace WebApplication1
             // Code that runs on application startup
            AreaRegistration.RegisterAllAreas();
            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
