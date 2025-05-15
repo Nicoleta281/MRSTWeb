db03b257d3a92bbd85ea6cec512a1953e762f057
Anastasia641 <anastasia.petrusca@isa.utm.md>
2025-05-15 12:24:21 +0300

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eUseControl.Domain
+namespace eUseControl.Helpers
 {
     public class Class1
     {
---
+++ PasswordHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace eUseControl.Helpers
+{
+     public static class PasswordHelper
+     {
+          public static string HashPassword(string password)
+          {
+               using (SHA256 sha256 = SHA256.Create())
+               {
+                    byte[] input = Encoding.UTF8.GetBytes(password);
+                    byte[] hash = sha256.ComputeHash(input);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLower();
+               }
+          }
+     }
+}
